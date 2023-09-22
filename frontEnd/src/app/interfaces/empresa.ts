bba165e05b8a15f144c478a69203eab632589edd
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-09-22 10:49:39 +0200

    El Crud de Angular separado de Draw IO

---
+++ frontEnd/src/app/interfaces/empresa.ts
@@ -0,0 +1,9 @@
+export interface Empresa {
+    id?:number;
+    usuario:string;   
+    telefono:string;    
+    poblacion:string;
+    dadoDeAlta:string;
+    
+
+}
---
+++ frontEnd/src/app/services/empresa.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Empresa } from '../interfaces/empresa';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpresaService {
+
+  private myAppUrl: string;
+  private myApiUrl: string;
+
+  constructor(private http: HttpClient) { 
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = 'api/usuarios/'
+
+  }
+
+  getListEmpresas(): Observable<Empresa[]>{
+    return this.http.get<Empresa[]>(`${this.myAppUrl}${this.myApiUrl}`);
+  }
+}
