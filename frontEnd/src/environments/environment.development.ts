b18d386f7ce6f46e8d7f665996ccc292ef34e150
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-10-25 11:28:17 +0200

    exquema estado del crud

--- frontEnd/src/environments/environment.development.ts
+++ frontEnd/src/environments/environment.development.ts
@@ -3,16 +3,9 @@
 
 export const environment = {
     production: false,
-     endpoint: 'http://localhost:3000/',
-    // endpoint: 'https://16.171.94.45:3000/',
+    // endpoint: 'http://localhost:3000/',
+     endpoint: 'https://16.171.94.45:3000/',
 };
 
 
 
-/*
-export const environment = {
-    production: false,
-    endpoint: 'http://crud360kbcn.eu-4.evennode.com/',
-};
-
-*/
