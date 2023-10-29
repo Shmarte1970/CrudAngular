e3247567e007678bc2afff89d59630f4e1380ad6
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-10-29 13:19:47 +0100

    el backend

--- frontEnd/src/environments/environment.ts
+++ frontEnd/src/environments/environment.ts
@@ -3,8 +3,8 @@
 
 export const environment = {
     production: false,
-    // endpoint :'http://localhost:3000/'
-   endpoint: 'https://16.171.94.45:3000/',
+     endpoint :'http://127.0.0.1:3000/'
+   //endpoint: 'https://16.171.94.45:3000/',
 };
 
 
