7d91aa898382fad253e33f42ada622de373ae805
Shmarte1970 <shmarte@gmail.com>
2023-11-13 07:57:14 +0100

    igualar el repositorio

--- frontEnd/src/environments/environment.development.ts
+++ frontEnd/src/environments/environment.development.ts
@@ -3,7 +3,7 @@
 
 export const environment = {
     production: false,
-     endpoint: 'http://127.0.0.1:3000/',
+     endpoint: 'http://127.0.0.10:3000/',
     // endpoint: 'https://16.171.94.45:3000/',
 };
 
