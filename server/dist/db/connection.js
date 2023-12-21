a57113a5c3b732486fa41723ce2fc12075ce8215
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-12-21 07:27:05 +0100

    funciona con la bbdd del servidor del erp

--- server/dist/db/connection.js
+++ server/dist/db/connection.js
@@ -1,14 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('prusuarios', 'admin', 'admin2023', {
+const sequelize = new sequelize_1.Sequelize(
+  "prusuarios",
+  "admin",
+  "admin2023",
+  {
     /*Acceso a BBDD de AWS
     
     host: 'testbbdd.c00tjquuyvht.eu-north-1.rds.amazonaws.com',
     dialect: 'mysql'
   });
 */
-    host: 'localhost',
-    dialect: 'mysql',
-});
+    host: "192.168.0.16",
+    dialect: "mysql",
+  }
+);
 exports.default = sequelize;
--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,21 +1,27 @@
 import {Sequelize} from 'sequelize';
 
 
+  /* Acceso a BBDD de AWS
+
+const sequelize = new Sequelize('prUsuarios', 'root', 'root2023', {
+
+  host: 'testbbdd.c00tjquuyvht.eu-north-1.rds.amazonaws.com',
+  dialect: 'mysql'
+
+});
+
+export default sequelize;
+
+*/
+  /* Accesso a BBDD Local */
 
 const sequelize = new Sequelize('prusuarios', 'admin', 'admin2023', {
 
-    /*Acceso a BBDD de AWS 
-    
-    host: 'testbbdd.c00tjquuyvht.eu-north-1.rds.amazonaws.com',
-    dialect: 'mysql'
-  });
-*/
-  
-
-  host: 'localhost',
+  host: '192.168.0.12',
   dialect: 'mysql',
 
 });
 
-
   export default sequelize;
+
+
